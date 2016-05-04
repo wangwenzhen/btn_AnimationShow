0e11550efcb73b4ec8e14ada3ef0d262767c2495
Little.Daddy <18333618118@163.com>
2016-05-04 16:11:26 +0800

    Initial Commit

---
+++ btn_AnimationShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  btn_AnimationShow
+//
+//  Created by Little.Daddly on 16/5/4.
+//  Copyright © 2016年 Little.Daddly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
